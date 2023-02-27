7ae2b1276d76374168e99a184ce7e38a222f94cf
OmkarUtake <omkarutake@nimapinfotech.com>
2023-02-27 16:33:39 +0530

    Update Vesion 3

--- DataLayer/DTO/BookViewModel.cs
+++ DataLayer/DTO/BookViewModel.cs
@@ -4,6 +4,7 @@ namespace CORE.Model.DTO
 {
     public class BookViewModel
     {
+
         public string Title { get; set; }
         public string Description { get; set; }
         public bool IsRead { get; set; }
